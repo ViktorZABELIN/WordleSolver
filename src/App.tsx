a6e91f7f6b013ef26b0f563cb7ec909eda946acd
ViktorZABELIN <128626595+ViktorZABELIN@users.noreply.github.com>
2025-04-16 22:09:21 +0300

    add styles shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
       <h1>Wordle solver</h1>
-
+      <Button>Press knopka</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './components/ui/theme-provider.tsx'
+import { ModeToggle } from './components/ui/mode-toggle.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme"><ModeToggle/><App /></ThemeProvider>
   </StrictMode>,
 )
